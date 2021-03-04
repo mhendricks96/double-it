af190189b39f98df4dffac11ca94257e5343a661
Marleyman876 <grant_garfield@yahoo.com>
2021-03-03 22:00:45 -0800

    cleaned up js file, added layout for needed functions

--- js/app.js
+++ js/app.js
@@ -1,52 +1,53 @@
-'use strict';
+'use strict'
+// global variables 
 let button = document.getElementById('start');
 let doubleButton = document.getElementById('double');
 let closeButton = document.getElementById('close');
 var number = 5;
-//let score = number;
 
-// Place all global variables above this comment
-
+//Double it Function  
 
 function double() {
   number *= 2;
   console.log(number);
 }
-//double(); // this function doubles the number
 
 function rng() {
   let luck = Math.floor(Math.random() * (100 - 0) + 0);
   return luck;
 }
 
-//rng(); // this functions returns a random number between 0 - 100
-// we need to replace the while loop with an event listener and larger game loop.
 function gameLoop() {
-  // let game = true;
-  // while (game) { //this while loop is for testing
-  let lucky = rng(); // this is where the important part begins
+  let lucky = rng(); 
   if (lucky >= 40) {
     double();
   }
   if (lucky < 40) {
     number = 5;
-    // game = false;
-    console.log('try again');
-    // break;
+    console.log('You have Sucky Luck!!');
   }
 }
-// }
 
+// render game to index.html main
+function renderNumber(){
+  
+}
+
+//render function for writing to leader board
+function renderLeader(){
+
+}; 
+
+//event listeners 
 function doubleClick() {
   gameLoop();
 }
+function closeGame(){
+  'does something'
+}
+
 // Place all functions above this comment
 
 doubleButton.addEventListener('click', doubleClick);
 
-// function clickHandle() {
-//   double();
-
-// }
-
-// button.addEventListener('click', clickHandle);
+closeButton.addEventListener('click', closeGame);
