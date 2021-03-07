f87fe6fb6d12e84e9091294907af42a3e8875d28
mhendricks96 <mhendricks96@yahoo.com>
2021-03-06 19:13:46 -0800

    added ability to change difficulty

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 // Global variables
 let doubleButton = document.getElementById('double');
 let closeButton = document.getElementById('close');
+let difficultySelect = document.getElementById('difficulty');
 let number = 5;
 let div1 = document.createElement('div');
 let play = document.getElementById('play');
@@ -21,18 +22,48 @@ function rng() {
 }
 
 // uses random number to decide if user wins or loses.
-function gameLoop() {
+// game loop for hard mode
+function gameLoopHard() {
   let lucky = rng();
-  if (lucky >= 40) {
+  if (lucky >= 50) {
     double();
     renderNumber(div1);
     div1.textContent = number;
   }
-  if (lucky < 40) {
+  if (lucky < 50) {
     number = 5;
     div1.textContent = 'Start back at 0';
   }
 }
+
+//game loop for medium mode
+function gameLoopMedium() {
+  let lucky = rng();
+  if (lucky >= 32) {
+    double();
+    renderNumber(div1);
+    div1.textContent = number;
+  }
+  if (lucky < 32) {
+    number = 5;
+    div1.textContent = 'Start back at 0';
+  }
+}
+
+//game loop for easy mode
+function gameLoopEasy() {
+  let lucky = rng();
+  if (lucky >= 15) {
+    double();
+    renderNumber(div1);
+    div1.textContent = number;
+  }
+  if (lucky < 15) {
+    number = 5;
+    div1.textContent = 'Start back at 0';
+  }
+}
+
 
 // constructor to make each user who quits into an object.
 function UserId (name, score){
@@ -50,14 +81,25 @@ function renderNumber(div){
 
 //Event listeners
 
+
 // controls "double it" button.
 function doubleClick() {
-  gameLoop();
+  if (difficultySelect.options[difficultySelect.selectedIndex].value === 'easy'){
+    console.log('easy mode');
+    gameLoopEasy();
+  }else if (difficultySelect.options[difficultySelect.selectedIndex].value === 'medium'){
+    console.log('medium mode');
+    gameLoopMedium();
+  } else {
+    console.log('hard mode');
+    gameLoopHard();
+  }
 }
+
 
 //controls "back out" button.
 function closeGame(){
-   let userName = prompt('Whats your name, quitter?');
+  let userName = prompt('Whats your name, quitter?');
   let newUserId = new UserId(userName, number );
   let stringifiedArray = JSON.stringify(allUsersArray);
   localStorage.setItem('player', stringifiedArray);
